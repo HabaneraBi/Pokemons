605f7856c646f0616f026e53f33866dbd324eafd
HabaneraBi <beriya2016@mail.ru>
2025-05-08 19:15:28 +0300

    add components: Button, Paragraph from main

--- src/Pages/Pokemons/Pokemons.tsx
+++ src/Pages/Pokemons/Pokemons.tsx
@@ -3,8 +3,13 @@ import { Heading } from "../../Components/heading";
 
 const Pokemons: FC = () => {
   return (
+<<<<<<< HEAD
     <div className="flex justify-center mt-6">
       <Heading>Lets Catch Pokemon...</Heading>
+=======
+    <div>
+      <Heading size="2xl">Lets Catch Pokemon...</Heading>
+>>>>>>> 4515233 (add components: Button, Paragraph)
     </div>
   );
 };
