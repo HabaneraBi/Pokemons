451523394d64ecf3e6acd6d306dffb5cec5c9d68
HabaneraBi <beriya2016@mail.ru>
2025-05-08 19:15:28 +0300

    add components: Button, Paragraph

---
+++ src/Components/Button.tsx
@@ -0,0 +1,21 @@
+import type { FC, PropsWithChildren } from "react";
+interface ButtonProps {
+  width: string;
+  padding?: string;
+}
+
+const Button: FC<PropsWithChildren<ButtonProps>> = ({
+  width,
+  padding,
+  children,
+}) => {
+  return (
+    <button
+      className={`${width} ${padding} cursor-pointer text-center text-lg text-white bg-[#24282b] rounded-md transition-colors duration-150 ease-in hover:bg-[#343638] active:bg-[#181b1d] active:transition-none`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export { Button };
