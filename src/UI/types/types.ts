53b20df0faaaa5218c1a4b8ed4c5148e7eaaed61
HabaneraBi <beriya2016@mail.ru>
2025-05-10 19:03:15 +0300

    add types for PokemonCard

---
+++ src/UI/types/types.ts
@@ -0,0 +1,10 @@
+interface FullPokemonInfo {
+  name: string;
+  abilities: string[];
+  imageUrl: string;
+  height: number;
+  weight: number;
+  speed: number;
+}
+
+export type { FullPokemonInfo };
