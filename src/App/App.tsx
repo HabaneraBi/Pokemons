f8135113f5d1a2b8c1639d435dbac2a6f236f986
Verus188 <king_king_03@bk.ru>
2025-05-10 15:04:10 +0300

    pages don't reset when you refresh the page

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
 import type { GlobalContext } from "./types";
 import { Header } from "../Modules/Header/Header";
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 import { Pokemons } from "../Pages/Pokemons/Pokemons";
 import { Home } from "../Pages/Home/Home";
 
@@ -10,8 +10,28 @@ const globalContext = createContext<GlobalContext>({
   setOpenTab: () => "",
 });
 
+const PAGES = ["home", "pokemons"] as const;
+type pages = (typeof PAGES)[number];
+
 const App: FC = () => {
-  const [openTab, setOpenTab] = useState<"home" | "pokemons">("pokemons");
+  const [openTab, setOpenTab] = useState<pages>("pokemons");
+
+  useEffect(() => {
+    const sessionPage = sessionStorage.getItem("page");
+    if (
+      sessionPage &&
+      openTab !== sessionPage &&
+      PAGES.includes(sessionPage as pages)
+    ) {
+      setOpenTab(sessionPage as pages);
+    } else {
+      sessionStorage.setItem("page", openTab);
+    }
+  }, []);
+
+  useEffect(() => {
+    sessionStorage.setItem("page", openTab);
+  }, [openTab]);
 
   return (
     <globalContext.Provider value={{ openTab, setOpenTab }}>
