650825bbc41cfb7f5de7473c44cd042f1ece92e5
HabaneraBi <beriya2016@mail.ru>
2025-05-13 18:13:17 +0300

    add onClick props for Button

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,14 +1,17 @@
 import type { FC, PropsWithChildren } from "react";
 interface ButtonProps {
   className?: string;
+  onClick?: () => void;
 }
 
 const Button: FC<PropsWithChildren<ButtonProps>> = ({
   className,
   children,
+  onClick,
 }) => {
   return (
     <button
+      onClick={onClick}
       className={`${className} cursor-pointer text-center text-lg text-white bg-[#24282b] rounded-md transition-colors duration-150 ease-in hover:bg-[#343638] active:bg-[#181b1d] active:transition-none`}
     >
       {children}
